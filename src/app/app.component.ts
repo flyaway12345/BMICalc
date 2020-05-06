8a0e180e60d8aebaf9eab699c4c737dcffe55c1a
Evan Haggard <evanhaggard123@gmail.com>
2020-05-06 11:50:18 -0400

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BMICalc'; 
-  feet: number = null;
-  inch: number = null;
-  weight: number = null;
+  answer: any;
+  feet: number;
+  inch: number;
+  weight: number;
   metricSwitch: boolean = false;
-  meter: number = null;
-  kilogram: number = null;
+  meter: number;
+  kilogram: number;
   
   calc(){
-    if(this.metricSwitch){
-    let meter = ((this.feet * 12)+this.inch) * 0.0254;
-    let kilogram = this.weight * 0.45359237;
+    let meter;
+    let kilogram;
+    
+    if(!this.metricSwitch){
+    meter = ((this.feet * 12 +this.inch))
+    kilogram = this.weight;
+    console.log(meter + " " + kilogram);
+    this.answer = (Math.round((kilogram/(Math.pow(meter,2))*703) * 100))/100;
     }else{
-      let meter = this.meter;
-      let kilogram = this.kilogram;
+      
+      
+      this.answer = (Math.round((this.kilogram/(Math.pow(this.meter/100,2))) * 100))/100;     
     }
-    return Math.round(((this.kilogram/(Math.pow(this.meter,2)))*100));
+    
   }
+  
   switchDisplay(){
     this.metricSwitch = !this.metricSwitch;
+    this.answer = '';
+    console.log(`switch to ${this.metricSwitch}`)
+
   }
 }
